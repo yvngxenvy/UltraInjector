6b4a924fab379356ddba5f91379358b898661cb5
yvngxenvy <yvngxenvyrl@gmail.com>
2025-03-14 20:09:30 -0700

    removed console

--- main.cpp
+++ main.cpp
@@ -3,12 +3,6 @@
 #include <iostream>
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-	// Console for testing
-	AllocConsole();
-	FILE* f;
-	freopen_s(&f, "CONOUT$", "w", stdout);
-	freopen_s(&f, "CONIN$", "r", stdin);
-
 	UserInterface window;
 	window.Initialize();
 	window.Destroy();
